0dca23ff01e84f93c8b0446fcacb0a15ffaa9dae
nebulousGirl <contact@nebulousweb.com>
2015-06-20 21:46:18 -0400

    Create Request from globals if not in container / Fixes #35

--- src/Strategy/RequestResponseStrategy.php
+++ src/Strategy/RequestResponseStrategy.php
@@ -13,7 +13,7 @@ class RequestResponseStrategy extends AbstractStrategy implements StrategyInterf
     public function dispatch($controller, array $vars)
     {
         $response = $this->invokeController($controller, [
-            $this->getContainer()->get('Symfony\Component\HttpFoundation\Request'),
+            $this->getRequest(),
             $this->getContainer()->get('Symfony\Component\HttpFoundation\Response'),
             $vars
         ]);
@@ -26,5 +26,15 @@ class RequestResponseStrategy extends AbstractStrategy implements StrategyInterf
             'When using the Request -> Response Strategy your controller must ' .
             'return an instance of [Symfony\Component\HttpFoundation\Response]'
         );
+    }
+
+    protected function getRequest()
+    {
+        if (! $this->getContainer()->isRegistered('Symfony\Component\HttpFoundation\Request') &&
+            ! $this->getContainer()->isInServiceProvider('Symfony\Component\HttpFoundation\Request') ) {
+            return $this->getContainer()->get('Symfony\Component\HttpFoundation\Request')->createFromGlobals();
+        } else {
+            return $this->getContainer()->get('Symfony\Component\HttpFoundation\Request');
+        }
     }
 }
